3f28651a1b431382598b25a4e9cadf3436e66479
lhm0611a-arch <lhm0611a@gmail.com>
2026-06-29 17:53:07 +0900

    Delete fix_data.cjs

--- fix_data.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/data.ts', 'utf8');
-
-content = content.replace(/\];스트레스를 긍정적으로[\s\S]*?\];/g, '];');
-
-fs.writeFileSync('src/data.ts', content);
-console.log('Fixed syntax error');
